92e62803484766c7582c4342fc0a7a13660643fb
Bafirgenius <bafirgenius@gmail.com>
2022-11-29 10:14:31 +0700

    before centralize

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
 } from "firebase/auth";
 
 import {
@@ -88,9 +90,10 @@ export const signInAuthUserWithEmailAndPassword = async (
 ) => {
     if (!email || !password) return;
 
-    return await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-    );
+    return await signInWithEmailAndPassword(auth, email, password);
 };
+export const signOutUser = async () => {
+    await signOut(auth);
+};
+export const onAuthStateChangedListener = (callback) =>
+    onAuthStateChanged(auth, callback);
